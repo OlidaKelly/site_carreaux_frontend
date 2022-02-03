9af1f7410b051bef511c24a4f35d88b96cf06e08
kelly <kelly.olida.pro@gmail.com>
2022-02-03 17:33:22 +0100

    creation of the form and contact page in progress

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Form from '../../components/Form/Form';
+import { BsWhatsapp } from 'react-icons/bs';
+import { MdOutlineEmail } from 'react-icons/md';
+import axios from 'axios';
+
 import './ContactPage.css';
 
 const ContactPage = () => {
+  const [about, setAbout] = useState();
+
+  useEffect(() => {
+  axios.get('http://localhost:5001/about/1').then(({ data }) => {
+    setAbout(data);
+  });
+  }, []);
+
   return (
     <div className="ContactPage">
       <h1>Nous contacter</h1>
+      <div className="div-ContactPage-container">
+        <div className="infos">
+          <ul>
+            <Link to={`tel:${about?.phone}`}>
+              <li>
+               <BsWhatsapp className='contact-icon'/>
+                {about?.phone}
+              </li>
+            </Link>
+            <li>
+              <MdOutlineEmail className='contact-icon' />
+              {about?.email}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <Form/>
     </div>
   );
 }
